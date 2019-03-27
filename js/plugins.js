11bcc73f3cbfd7f875997d5eb78f35a9ae3486d9
Mohamed <az.mohamedayman12@gmail.com>
2019-03-27 23:37:52 +0200

    first commit

---
+++ js/plugins.js
@@ -0,0 +1,115 @@
+/*global $, alert, console*/
+
+$(function () {
+    'use strict';
+    
+    // Adjust Header Height
+    
+    var myHeader = $('.header'),
+        mySlider = $('.bxslider');
+    
+    myHeader.height($(window).height());
+    
+    $(window).resize(function () {
+        
+        myHeader.height($(window).height());
+        
+            // adjust bxslider to center screen 
+
+        mySlider.each(function () {
+
+            $(this).css('paddingTop', ($(window).height() - $('.bxslider li').height()) / 2);
+
+        });
+
+    });
+    
+    // Links Add Active class
+    
+    $('.links li a').click(function () {
+        
+        $(this).parent().addClass('active').siblings().removeClass('active');
+        
+    });
+    
+    // adjust bxslider to center screen 
+    
+    mySlider.each(function () {
+        
+        $(this).css('paddingTop', ($(window).height() - $('.bxslider li').height()) / 2);
+        
+    });
+    
+    
+    // Trigger bx Slider 
+    
+    mySlider.bxSlider({
+        
+        pager: false
+        
+    });
+    
+    
+    // Smooth Scroll
+    
+    $('.links li a').click(function () {
+       
+        $('html , body').animate({
+           
+            scrollTop: $('#' + $(this).data('value')).offset().top
+            
+        }, 1000);
+        
+    });
+    
+    // Our Own Slider Code 
+    
+    (function autoSlider() {
+        
+        $('.slider .active').each(function () {
+            
+            if (!$(this).is(':last-child')) {
+            
+                $(this).delay(3000).fadeOut(1000, function () {
+               
+                    $(this).removeClass('active').next().addClass('active').fadeIn();
+                
+                    autoSlider();
+                
+                });
+            
+            } else {
+            
+                $(this).delay(3000).fadeOut(1000, function () {
+               
+                    $(this).removeClass('active');
+                
+                    $('.slider div').eq(0).addClass('active').fadeIn();
+                
+                    autoSlider();
+                
+                });
+            }
+        
+        });
+      
+    }());
+    
+    //Trigger MixItUp Shuffle
+    
+    $(function () {
+        
+        $('#shuffle').mixItUp();
+        
+    });
+    
+    // Adjust Shuffle Click
+    
+    
+    $('ul li').click(function () {
+       
+        $(this).addClass('selected').siblings().removeClass('selected');
+        
+    });
+    
+});
